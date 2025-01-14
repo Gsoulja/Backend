86d3339cab1600cef61840ee3dd9e9d5b38c5c7d
Glody Figueiredo <glodyfigueiredo@outlook.com>
2025-01-14 12:57:01 +0100

    init commit

---
+++ src/main/java/com/kitandasmart/kitandasmartBE/KitandasmartBeApplication.java
@@ -0,0 +1,13 @@
+package com.kitandasmart.kitandasmartBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitandasmartBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitandasmartBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kitandasmart/kitandasmartBE/KitandasmartBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kitandasmart.kitandasmartBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitandasmartBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
